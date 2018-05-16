d0cd3209ac6de928bc1255fdd3b8d7aef01f73aa
santiihoyos <santiago.hoyos93@gmail.com>
2018-05-16 19:29:05 +0200

    first

---
+++ Tercera persona/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  public GameObject Target;
+  public float Xoffset;
+  public float Yoffset;
+  public float Zoffset;
+
+  private Vector3 _positionReal = new Vector3();
+
+  private void LateUpdate()
+  {
+    Vector3 targetPos = Target.transform.position;
+    _positionReal.x = targetPos.x + Xoffset;
+    _positionReal.y = targetPos.y + Yoffset;
+    _positionReal.z = targetPos.z + Zoffset;
+    transform.position = _positionReal;
+  }
+}
---
+++ Tercera persona/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+  public GameObject MarcadorDestino;
+  public LayerMask caminable;
+  public LayerMask seleccionable;
+
+  // Update is called once per frame
+  void Update()
+  {
+    Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    Debug.DrawRay(mouseRay.origin, mouseRay.direction * 5000, Color.green);
+    RaycastHit hit;
+
+    if (Input.GetMouseButtonDown(1))
+    {
+      bool hasHit = Physics.Raycast(mouseRay, out hit, 5000);
+      if (hasHit)
+      {
+        //forma teradicional
+        //var mascaraChocado = LayerMask.GetMask(LayerMask.LayerToName(hit.collider.gameObject.layer));
+
+        //si tenemos en cuenta qeu es una potencia de 2 es facil calcularla
+        var mascaraChocado = (int)Mathf.Pow(2, hit.collider.gameObject.layer);
+
+        if ((mascaraChocado & caminable.value) == mascaraChocado)
+        {
+          MarcadorDestino.transform.position = hit.point;
+          GetComponent<AICharacterControl>().target = MarcadorDestino.transform;
+        }
+        else
+        {
+          print("Click en otra layer");
+        }
+      }
+    }
+  }
+}
