4e7a3fb9d03d7d3ee4003fd1772b7b8dc7d473c0
7isl3kpx0 <santiihoyos>
2018-05-23 20:56:14 +0200

    trabajando en el enemogio

--- Tercera persona/Assets/EnemigoController.cs
+++ Tercera persona/Assets/EnemigoController.cs
@@ -6,14 +6,18 @@ public class EnemigoController : MonoBehaviour
 {
     public Renderer Renderer;
 
+    private Animator _animator;
+    
     // Use this for initialization
     void Start()
     {
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 
     private void OnMouseEnter()
@@ -29,6 +33,16 @@ public class EnemigoController : MonoBehaviour
         if (Renderer != null)
         {
             Renderer.materials[1].SetFloat("_Outline", 0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("atacando!");
+            transform.LookAt(other.transform);
+            _animator.SetTrigger("ataca");
         }
     }
 }
